60f9caa37e33adbc40419b4126da09aee0ff75a9
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2022-09-22 21:58:34 +0100

    added code to remove empty directories

--- Main.py
+++ Main.py
@@ -4,17 +4,33 @@ import os
 import hashlib
 from send2trash import send2trash
 
+
+
 Tk().withdraw() # Don't need full GUI, so stops root window from appearing 
 path = askdirectory (title = 'Please Select A Folder')
 
-walker = os.walk(path)
-uniqueFiles = dict()
-for folder, sub_folder, files in walker:
-    for file in files:
-        filepath = os.path.join(folder,file)
-        filehash = hashlib.md5(open(filepath,'rb').read()).hexdigest()
-        if filehash in uniqueFiles:
-            send2trash(filepath)
-            print (f'{filepath} Has Been Deleted')
-        else:
-            uniqueFiles[filehash] = path   
+def smash_dupe_files():
+    walker1 = os.walk(path, topdown=False)
+    uniqueFiles = dict()
+    for folder, sub_folder, files in walker1:
+        for file in files:
+            filepath = os.path.join(folder,file)
+            filehash = hashlib.md5(open(filepath,'rb').read()).hexdigest()
+            if filehash in uniqueFiles:
+                send2trash(filepath)
+                print (f'{filepath} Has Been Deleted')
+            else:
+                uniqueFiles[filehash] = path   
+    if len(os.listdir(path)) == 0:
+        os.rmdir(path)
+
+
+
+    
+
+
+
+if  __name__ == '__main__':
+    smash_dupe_files()
+    
+    print ('***Operation complete. Please check recycle bin for deleted data before permenant removal')
